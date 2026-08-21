776f54642c5d2c528a4bf3a38410bb2e825c3378
nboob <nikhil.boob@oracle.com>
2026-08-21 19:32:56 +0530

    Initial Spring Boot load test service

---
+++ src/main/java/com/example/loadtest/LoadTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.loadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadTestServiceApplication.class, args);
+	}
+
+}
